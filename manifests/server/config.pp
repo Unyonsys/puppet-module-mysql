026374a80c46a9a615b2f7cc5cce3c1ee5fd551a
Bruno Léon <bruno.leon@unyonsys.com>
2012-09-14 20:43:07 +0200

    Fix missing my.cnf in Percona cluster mode.

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -5,20 +5,25 @@ class mysql::server::config (
   $mysql_config_options,
 ) {
   if ! $use_percona_pkg {
-    if versioncmp($::operatingsystemrelease, '11.10') > 0 {
-      $lc_messages_dir = true
-    }
-    else {
-      $lc_messages_dir = false
-    }
-    file { "${mysql::variables::mysql_root}/my.cnf":
-      ensure  => file,
-      content => template( "${module_name}/my.cnf.erb" ),
-    }
+    $my_cnf = "${module_name}/my.cnf.erb"
+  }
+  else {
+    $my_cnf = "${module_name}/my.cnf_percona.erb"
+  }
+
+  if versioncmp($::operatingsystemrelease, '11.10') > 0 {
+    $lc_messages_dir = true
+  }
+  else {
+    $lc_messages_dir = false
+  }
+  file { "${mysql::variables::mysql_root}/my.cnf":
+    ensure  => file,
+    content => template( $my_cnf ),
   }
 
   file { "${mysql::variables::mysql_root}/conf.d/options.cnf":
-    ensure => file,
+    ensure  => file,
     content => template( "${module_name}/options.cnf.erb" ),
   }
 }
